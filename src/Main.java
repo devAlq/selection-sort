2ef2a5596138d343ddbfaebaba5bcdb29086fdf0
devAlq <khalidAlq6@gmail.com>
2024-06-30 13:46:30 +0300

    taskDone

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
 public class Main {
 
- static void main(String[] args){
+  static void main(String[] args){
+      int[] array = {
+          90,70,10,1,0,30
+      };
+      int[]sortedArray = selectionSort(array);
+      for(int x =0; x<sortedArray.length;x++){
+          System.out.print(sortedArray[x]+ " ");
+      }System.out.println();
+ 
+   }
+   public static int[] selectionSort(int[] array){
+      for(int i = 0 ; i<array.length - 1; i++){
+          int minIndex = i;
+          for(int j = i+1; j < array.length; j++){
+              if(array[j] < array[minIndex]){
+                  minIndex = j;
+              }
 
-          /* your code here */
-  }
-}
+          }
+      
+      int value = array[minIndex];
+      array[minIndex] = array[i];
+      array[i] = value;
+   }return array;}
+ } 
